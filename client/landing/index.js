a4931d4124e35c64af4d178131bafeed857a4919
Mudpadans <philmodel.101@gmail.com>
2023-06-07 12:15:56 -0400

    sign-up initial

--- client/landing/index.js
+++
@@ -1,6 +0,0 @@
-const eventPage = document.getElementById("event-page")
-const forumPage = document.getElementById("forum-page")
-const signUpPage = document.getElementById("sign-up-page")
-
-
-eventPage.addEventListener('click',)
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
+const path = require('path')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/', express.static(path.join(__dirname, '../client')))
 
 const { PORT } = process.env;
 
